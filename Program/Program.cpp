f07e3eab58038ddd42281dbd18a6f8f08a529392
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-12-29 22:33:35 +0200

    Add showEnterSize function

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -3,35 +3,37 @@
 using namespace std;
 
 void showLoadingScreen() {
-    cout << "                                     # #  ( )" << endl;
-    cout << "                                  ___#_#___|__" << endl;
-    cout << "                              _  |____________|  _" << endl;
-    cout << "                       _=====| | |            | | |==== _" << endl;
-    cout << "                 =====| |.---------------------------. | |====" << endl;
-    cout << "   <--------------------'   .  .  .  .  .  .  .  .   '--------------/" << endl;
-    cout << "    \\                                                             /" << endl;
-    cout << "     \\_______________________________________________WWS_________/" << endl;
-    cout << "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
-    cout << "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
-    cout << "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
+	cout << "                                     # #  ( )" << endl;
+	cout << "                                  ___#_#___|__" << endl;
+	cout << "                              _  |____________|  _" << endl;
+	cout << "                       _=====| | |            | | |==== _" << endl;
+	cout << "                 =====| |.---------------------------. | |====" << endl;
+	cout << "   <--------------------'   .  .  .  .  .  .  .  .   '--------------/" << endl;
+	cout << "    \\                                                             /" << endl;
+	cout << "     \\___________________________________________________________/" << endl;
+	cout << "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
+	cout << "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
+	cout << "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
 
-    cout << "           _           _   _   _           _     _         " << endl;
-    cout << "          | |         | | | | | |         | |   (_)        " << endl;
-    cout << "          | |__   __ _| |_| |_| | ___  ___| |__  _ _ __   ___  " << endl;
-    cout << "          | '_ \\ / _` | __| __| |/ _ \\/ __| '_ \\| | '_ \\ / __|  " << endl;
-    cout << "          | |_) | (_| | |_| |_| |  __/\\__ \\ | | | | |_) |\\__ \\ " << endl;
-    cout << "          |_.__/ \\__,_|\\__|\\__|_|\\___||___/_| |_|_| .__/ |___/ " << endl;
-    cout << "                                                  | |         " << endl;
-    cout << "                                                  |_|         " << endl;
-    cout << "" << endl;
+	cout << "           _           _   _   _           _     _         " << endl;
+	cout << "          | |         | | | | | |         | |   (_)        " << endl;
+	cout << "          | |__   __ _| |_| |_| | ___  ___| |__  _ _ __   ___  " << endl;
+	cout << "          | '_ \\ / _` | __| __| |/ _ \\/ __| '_ \\| | '_ \\ / __|  " << endl;
+	cout << "          | |_) | (_| | |_| |_| |  __/\\__ \\ | | | | |_) |\\__ \\ " << endl;
+	cout << "          |_.__/ \\__,_|\\__|\\__|_|\\___||___/_| |_|_| .__/ |___/ " << endl;
+	cout << "                                                  | |         " << endl;
+	cout << "                                                  |_|         " << endl;
+	cout << "" << endl;
 
+}
+
+void showEnterSizeMessage() {
+	cout << "" << endl;
 }
 
 int main()
 {
-    showLoadingScreen();
-    
-    cout << "Press enter to start a game!";
-    cin.get();
-    cout << "Game started";
+	showLoadingScreen();
+	showEnterSizeMessage();
+	
 }
