69cf6558f442f0854790de1ed13e777864b7992e
MartiHr <81310884+MartiHr@users.noreply.github.com>
2023-12-29 15:18:08 +0200

    Add loading screen

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,8 +1,37 @@
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
+void showLoadingScreen() {
+    cout << "                                     # #  ( )" << endl;
+    cout << "                                  ___#_#___|__" << endl;
+    cout << "                              _  |____________|  _" << endl;
+    cout << "                       _=====| | |            | | |==== _" << endl;
+    cout << "                 =====| |.---------------------------. | |====" << endl;
+    cout << "   <--------------------'   .  .  .  .  .  .  .  .   '--------------/" << endl;
+    cout << "    \\                                                             /" << endl;
+    cout << "     \\_______________________________________________WWS_________/" << endl;
+    cout << "  wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
+    cout << "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
+    cout << "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww" << endl;
+
+    cout << "           _           _   _   _           _     _         " << endl;
+    cout << "          | |         | | | | | |         | |   (_)        " << endl;
+    cout << "          | |__   __ _| |_| |_| | ___  ___| |__  _ _ __   ___  " << endl;
+    cout << "          | '_ \\ / _` | __| __| |/ _ \\/ __| '_ \\| | '_ \\ / __|  " << endl;
+    cout << "          | |_) | (_| | |_| |_| |  __/\\__ \\ | | | | |_) |\\__ \\ " << endl;
+    cout << "          |_.__/ \\__,_|\\__|\\__|_|\\___||___/_| |_|_| .__/ |___/ " << endl;
+    cout << "                                                  | |         " << endl;
+    cout << "                                                  |_|         " << endl;
+    cout << "" << endl;
+
+}
+
 int main()
 {
-	cout << "Start";
+    showLoadingScreen();
+    
+    cout << "Press enter to start a game!";
+    cin.get();
+    cout << "Game started";
 }
